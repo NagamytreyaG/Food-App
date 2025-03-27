4d9b25e80830df65588e60cdab0e3989a9deb161
NagamytreyaG <NG185157@ncr.com>
2025-03-27 17:31:48 +0530

    Update Restaurant.java

--- Food-App/src/main/java/com/example/Food_App/Model/Restaurant.java
+++ Food-App/src/main/java/com/example/Food_App/Model/Restaurant.java
@@ -8,7 +8,7 @@ import org.springframework.data.mongodb.core.index.TextIndexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.List;
-
+//hello world
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
