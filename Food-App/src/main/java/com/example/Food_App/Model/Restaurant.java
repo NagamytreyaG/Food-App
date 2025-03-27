fcf64aa838591c5c1d926ee4b1bbc6ce19779867
GULUBHAM NAGAMYTREYA <ng185157@outlook.com>
2025-03-27 17:53:04 +0530

    removed comment 2

--- Food-App/src/main/java/com/example/Food_App/Model/Restaurant.java
+++ Food-App/src/main/java/com/example/Food_App/Model/Restaurant.java
@@ -8,6 +8,7 @@ import org.springframework.data.mongodb.core.index.TextIndexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.List;
+//new commit
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
